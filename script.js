534effad023bc2ac79d066d9e4a65a84b9c52675
石田 僚平 <ishida-r@global-asp.co.jp>
2025-11-14 18:11:55 +0900

    1st commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function play(playerHand){
+    const hands =['グー','チョキ','パー'];
+    const computerHand=hands[Math.floor(Math.random()*3)];
+    let result='';
+
+    if(playerHand==computerHand){
+        result ='あいこ';
+    }else if(    (playerHand === 'グー' && computerHand === 'チョキ') ||
+    (playerHand === 'チョキ' && computerHand === 'パー') ||
+    (playerHand === 'パー' && computerHand === 'グー')
+)
+{
+        result ='あなたの勝ち';
+    }else{
+        result ='あなたの負け';
+    }
+
+    document.getElementById('result').textContent=`あなた：${playerHand} / コンピューター：${computerHand} -> ${result}`;
+}
+
+function sayHello(){
+    alert("こんにちは");
+}
